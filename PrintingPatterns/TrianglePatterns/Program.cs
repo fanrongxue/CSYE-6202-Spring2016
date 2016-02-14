aeda9dd6a80fe5f158dfff154902fc7128a4e0a2
fanrongxue <xuefanrong0720@gmail.com>
2016-02-14 11:24:20 -0800

    Assignment2

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -14,24 +14,71 @@ namespace TrianglePatterns
 			Console.ReadLine();
 		}
 
-		static void DisplayPatternA()
-		{
-			// your implementation here
-		}
+        static void DisplayPatternA()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int k = 0; k < i + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
 
-		static void DisplayPatternB()
-		{
-			// your implementation here
-		}
+            // your implementation here
+        }
+        static void DisplayPatternB()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                for (int k = 0; k < i + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
 
-		static void DisplayPatternC()
-		{
-			// your implementation here
-		}
 
-		static void DisplayPatternD()
-		{
-			// your implementation here
-		}
-	}
+
+            // your implementation here
+        }
+
+        static void DisplayPatternC()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                for (int j = 0; j < 10 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < i + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            // your implementation here
+        }
+        static void DisplayPatternD()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < i + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            // your implementation here
+        }
+    }
 }
