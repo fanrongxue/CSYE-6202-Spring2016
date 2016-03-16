e868a3df1a25e5cd241b1dd5998c7ba99d3e4e90
fanrongxue <xuefanrong0720@gmail.com>
2016-03-16 12:19:01 -0700

    Assignment5

---
+++ PAY__ROLL/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+
+namespace UnitTestProject2
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+     
+        [TestFixtureSetUp]
+        public void SalariedEmployee()
+        {
+            // prepare
+            double eeklysalary;
+
+            // action
+            Program.CalculateTotalCost(Program.GasType.RegularGas, amount, ref totalCost);
+
+            // assert
+            Assert.That(expected, Is.EqualTo((int)totalCost));
+        }
+        [TestFixtureSetUp]
+        public void BasePlusCommission()
+        {
+            // prepare
+            double eeklysalary;
+
+            // action
+            Program.CalculateTotalCost(Program.GasType.RegularGas, amount, ref totalCost);
+
+            // assert
+            Assert.That(expected, Is.EqualTo((int)totalCost));
+        }
+    }
+}
